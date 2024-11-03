215d901119d45b0e06bf56dd2fa7b2db38f68dab
Marat Abrarov <abrarov@gmail.com>
2024-11-04 00:03:47 +0300

    Configurable hello message.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
+use std::process::ExitCode;
+use std::env::args;
 
-fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello, Rust!");
-    let width = message.chars().count();
-    let writer = BufWriter::new(stdout.lock());
-    let _ = say(message.as_ref(), width, writer);
+fn main() -> ExitCode {
+    let args: Vec<String> = args().collect();
+    let message: &str = if args.len() > 1 { &args[1] } else { "Hello, Rust!" };
+    let width = message.len();
+    let writer = BufWriter::new(stdout().lock());
+    if say(message, width, writer).is_err() {
+        return ExitCode::from(1);
+    }
+    ExitCode::from(0)
 }
